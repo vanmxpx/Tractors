92456b57ccc631107a603c486eeff0553bca09e6
Nikita Tsyhankov <nikita1996.tsyhankov@gmail.com>
2020-02-17 16:40:51 +0200

    new

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FetchDataComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: 'app' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
